7b02346b41ad08e49ba16ae8fcce994c4b22192c
stanislavets <stanislavets2009@mail.ru>
2024-06-24 22:05:53 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+
+number_tasks = 12
+number_hours = 1.5
+course_name = "Python"
+time_one_task = (number_hours/number_tasks)
+print( "Курс:",course_name,",","всего задач:",number_tasks,"затрачено часов:",number_hours,"среднее время выполнения:", time_one_task, "часа" )
